911faf5cc742e0dcc857469aad7fc23d2f6e71fd
Karine <karine-caldas@hotmail.com>
2025-11-10 00:31:53 -0300

    Adicionando nome da equipe

--- src/exception/ContatoNaoEncontradoException.java
+++ src/exception/ContatoNaoEncontradoException.java
@@ -1,3 +1,13 @@
+/* 
+ *  Equipe: 
+
+   - José Paulo Cauás Tenório 
+   - Juliana Dias da Silva 
+   - Karine Viana Caldas da Silva 
+   - Petrus Oliveira de Andrade Cruz 
+   - Thiago Nicolas Ananias da Silva
+ */
+
 package exception;
 
 public class ContatoNaoEncontradoException extends Exception {
